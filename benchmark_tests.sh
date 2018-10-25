f592df5c8ddcb3d7faa398425e34b49110782411
rakeshmadivi <44462799+rakeshmadivi@users.noreply.github.com>
2018-10-25 16:44:11 +0530

    Update benchmark_tests.sh

--- benchmark_tests.sh
+++ benchmark_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # This scripts works for sysbench version: 0.4.12
 # GLOBAL VALUES
-ncpus=`nprocs`
+ncpus=`nproc`
 
 function sysbench_tests()
 {
   echo which test to perform?
-  echo -e 1. CPU \n2. MEMORY
+  echo -e "1. CPU \n2. MEMORY"
   echo Enter:
   read op
   if [ "$op" = "1" ]; then
@@ -14,11 +14,14 @@ function sysbench_tests()
     outfile=sysbench_cpu.txt
     init=10000
     
+     rm -rf $outfile
+    
     st=$SECONDS
     for((mx=$init; mx<=$init*10; mx*=2))
     do
       for((th=1; th<=$ncpus; th*=2))
       do
+        echo "\nRunning for PR: $mx TH: $th Configuration"
         echo PR:$mx TH:$th >> $outfile
         sysbench --test=cpu --cpu-max-prime=$mx --num-threads=$th run >> $outfile
       done
@@ -29,14 +32,20 @@ function sysbench_tests()
     
   elif [ "$op" = "2" ]; then
     echo Running MEMORY Workload Benchmark...
+    outfile=sysbench_memory.txt
+    
     init=10000
     
     # Trying to allocate memory more than L3 Cache and stretch to RAM
     memload=250M
-    totalmem=200G
+    totalmem=100G
+    
+    rm -rf $outfile
+    
     st=$SECONDS
-    for((th=1; th<=`nprocs`; th*=2))
+    for((th=1; th<=$ncpus; th*=2))
     do
+        echo "Running with MEMLOAD: $memload, TOTALMEM: $totalmem, THREADS: $th"
         echo TH:$th >> $outfile
         # --memory-scope=global/local --memory-oper=read/write/none
         sysbench --test=memory --memory-block-size=$memload --memory-total-size=$totalmem --memory-scope=global --memory-oper=read --num-threads=$th run >> $outfile
