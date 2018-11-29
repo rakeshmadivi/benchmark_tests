486adfd7c87a470fcc58cf09079d0a940bd36934
rakeshmadivi <44462799+rakeshmadivi@users.noreply.github.com>
2018-11-29 13:10:40 +0530

    Update benchmark_tests.sh

--- benchmark_tests.sh
+++ benchmark_tests.sh
@@ -517,14 +517,31 @@ function redis_tests()
 # SPEC
 function speccpu_tests()
 {
-  echo -e "Please enter SPECCPU-2017 Installed Location(Full path): "
-  read speccpu_home #=$HOME/spec2017_install/
+  default_loc=${HOME}/spec2017
+  echo -e "SPEC HOME Location: $default_loc \nIs above location correct?(y/n): "
+  read confirm
+  if [ "$confirm" = "y" ];then
+    speccpu_home=$default_loc
+  else
+    echo -e "Please enter SPECCPU-2017 Installed Location(Full path): "
+    read speccpu_home #=$HOME/spec2017_install/
+  fi
   
+  echo -e "SELECTED SPEC HOME LOCATION: ${speccpu_home}\n"
   echo Sourcing SPEC Environment...
   cd $speccpu_home && source shrc
   
-  echo -e "Enter config  file name: "
-  read cfgfile
+  cd config
+  cfg_list=($(echo `ls *.cfg`))
+  for i in $(seq 0 $(( ${#cfg_list[*]} - 1)) )
+  do
+    echo $i ${cfg_list[$i]}      
+  done
+  
+  echo -e "Enter config file index: "
+  read cfg_option
+  cfgfile=$cfg_list[$cfg_option]
+  
   #=$1
   copies=$ncpus
   threads=1
@@ -537,10 +554,10 @@ function speccpu_tests()
   
   echo ULIMIT: `ulimit -s`
   
-  echo -e "\nUSING: $cfgfile\n"
-  cd $speccpu_home/
-  source $speccpu_home/shrc
-  cd config
+  echo -e "\nUSING CONFIGURATION FILE: $cfgfile [ PATH: $PWD ] \n"
+  #cd $speccpu_home/
+  #source $speccpu_home/shrc
+  #cd config
   
   declare -a spectests=("intrate" "fprate" "intspeed" "fpspeed")
   for i in "${spectests[@]}"
@@ -551,7 +568,7 @@ function speccpu_tests()
         copies=1
         threads=$th_per_core
         
-        echo -e "RUN: $i CONFIG: $cfgfile \nCOPIES: $copies THREADS: $threads"
+        echo -e "RUN: $i \nCONFIG: $cfgfile \nCOPIES: $copies THREADS: $threads"
         
         pin=`cat /sys/devices/system/cpu/cpu1/topology/thread_siblings_list`
         echo -e "Running $i with PINNING: ${pin}"
@@ -563,7 +580,7 @@ function speccpu_tests()
       else
         copies=$ncpus
         threads=1
-        echo -e "RUN: $i CONFIG: $cfgfile \nCOPIES: $copies THREADS: $threads"
+        echo -e "RUN: $i \nCONFIG: $cfgfile \nCOPIES: $copies THREADS: $threads"
         time runcpu -c $cfgfile --tune=base --copies=$copies --threads=$threads --reportable $i
       fi
     en=$SECONDS
